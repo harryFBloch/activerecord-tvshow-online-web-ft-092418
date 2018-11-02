f2141f020406108cdff6111658128299a20eb9e6
Harry Bloch <harryfbloch@gmail.com>
2018-11-02 12:58:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   
   
   def self.highest_rating
-    Show.order(rating: :desc).first.rating
+    self.order(rating: :desc).first.rating
   end
   
   def self.most_popular_show
